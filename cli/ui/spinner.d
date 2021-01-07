fbfa693a3b80bf049a3253b68deecd9f45715b92
DarkHole1 <dark.hole1@yandex.ru>
2021-01-07 15:34:34 +0300

    Implemented method for waiting spinners

--- cli/ui/spinner.d
+++ cli/ui/spinner.d
@@ -7,6 +7,8 @@ import std.conv: to;
 import std.datetime;
 import cli.ui.os;
 import cli.ui.color;
+import cli.ui.ansi;
+import std.stdio: write;
 
 class SpinGroup {
   Mutex m;
@@ -36,7 +38,46 @@ class SpinGroup {
   }
 
   auto wait() {
+    auto idx = 0;
+    while(true) {
+      auto allDone = true;
 
+      // TODO: Implement Terminal
+      // auto width = Terminal.width;
+      auto width = 80;
+
+      this.m.lock_nothrow();
+      // TODO: Implement raw?
+      foreach(intIndex, task; this.tasks) {
+        auto natIndex = intIndex + 1;
+        auto taskDone = task.check;
+        if(!taskDone) {
+          allDone = false;
+        }
+
+        if(natIndex > this.consumedLines) {
+          write(task.render(idx, true, width), "\n");
+          this.consumedLines += 1;
+        } else {
+          auto offset = this.consumedLines - intIndex;
+          auto moveTo = ANSI.cursorUp(offset) ~ "\r";
+          auto moveFrom = "\r" ~ ANSI.cursorDown(offset);
+          write(moveTo, task.render(idx, idx == 0, width), moveFrom);
+        }
+      }
+      this.m.unlock_nothrow();
+
+      if(allDone) {
+        break;
+      }
+
+      idx = (idx + 1) % Spinner.GLYPHS.length;
+      Spinner.index = idx;
+      Thread.sleep(Spinner.PERIOD);
+    }
+
+    // TODO: Implement error checking
+    return true;
   }
 
   auto debrief() {
