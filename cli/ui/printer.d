fcc251e545ae76de5f64012ebe7616862f800780
DarkHole1 <dark.hole1@yandex.ru>
2021-01-23 20:03:48 +0300

    Fixed errors

--- cli/ui/printer.d
+++ cli/ui/printer.d
@@ -3,10 +3,11 @@ import cli.ui.color: Color;
 import std.stdio: File, stdout, writeln;
 
 class Printer {
-  bool puts(string msg, bool format = true, Color frameColor = null, bool graceful = true, File to = stdout) {
+  static auto puts(string msg, bool format = true, Color frameColor = null, bool graceful = true, File to = stdout) {
     // TODO: Implement formatting
     // TODO: Implement coloring
     // TODO: Implement graceful
     to.writeln(msg);
+    return true;
   }
 }
