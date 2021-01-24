f4e15df21f8fbd1bec3cc39631f692ace782eefb
DarkHole1 <dark.hole1@yandex.ru>
2021-01-24 14:32:49 +0300

    Added width and height

--- cli/ui/terminal.d
+++ cli/ui/terminal.d
@@ -9,12 +9,20 @@ class Terminal {
   static const ushort DEFAULT_WIDTH = 80;
   static const ushort DEFAULT_HEIGHT = 24;
 
+  static auto width() {
+    return this.winsize[1];
+  }
+
+  static auto height() {
+    return this.winsize[0];
+  }
+
   static auto winsize() {
     wsz sz;
     auto res = ioctl(stdout.fileno(), TIOCGWINSZ, &sz);
     if(res != 0) {
-      return tuple(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+      return tuple(DEFAULT_HEIGHT, DEFAULT_WIDTH);
     }
-    return tuple(cast(const)sz.ws_col, cast(const)sz.ws_row);
+    return tuple(cast(const)sz.ws_row, cast(const)sz.ws_col);
   }
 }
